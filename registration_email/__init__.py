854b022e09e7721a8b68c5dd21ba27bf12c39e5f
Martin Brochhaus <mbrochh@gmail.com>
2013-03-30 16:04:30 +0800

    Released v0.5.1

--- registration_email/__init__.py
+++ registration_email/__init__.py
@@ -1,7 +1,2 @@
 # -*- coding: utf-8 -*-
-VERSION = (0, 5, 'final')
-
-if VERSION[-1] != "final": # pragma: no cover
-    __version__ = '.'.join(map(str, VERSION))
-else: # pragma: no cover
-    __version__ = '.'.join(map(str, VERSION[:-1]))
+__version__ = '0.5.1'
